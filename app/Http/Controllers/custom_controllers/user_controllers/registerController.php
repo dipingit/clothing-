943edb4cb9bbbe79c0d19051c1e153bc46c487f5
Dip Roy <diproyf87@gmail.com>
2024-04-04 04:18:00 -0300

    strengthen the password authentication by adding regex rules

--- app/Http/Controllers/custom_controllers/user_controllers/registerController.php
+++ app/Http/Controllers/custom_controllers/user_controllers/registerController.php
@@ -26,9 +26,19 @@ class registerController extends Controller
             $user = new User;
             $validatedData = $request->validate([
                 'username' => 'required|string|max:50|alpha_dash',
-                'password' => 'required|string|min:8'
-
-            ]);
+                'password' => [
+                    'required',
+                    'string',
+                    'min:8',             // must be at least 8 characters in length
+                    'regex:/[a-z]/',      // must contain at least one lowercase letter
+                    'regex:/[A-Z]/',      // must contain at least one uppercase letter
+                    'regex:/[0-9]/',      // must contain at least one digit
+                    'regex:/[@$!%*#?&]/', // must contain a special character
+                ],
+            ],
+                [
+                    'password.regex' => 'The password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character.'
+                ]);
             if($validatedData){
                 $user->username = $validatedData['username'];
                 $user->password = Hash::make($validatedData['password']);
